4e9f963397abf5c90ae244f4adcdae9e607c5c01
wyqbupt <wyqcsnet@yeah.net>
2015-09-04 11:32:47 +0800

    complete CheckerBoard

--- Factory-method/Factory.py
+++ Factory-method/Factory.py
@@ -2,6 +2,9 @@
 
 import sys
 import io
+import os
+import tempfile
+
 BLACK, WHITE = ("BLACK", "WHITE")
 
 if sys.platform.startswith("win"):
@@ -13,9 +16,19 @@ else:
         return "\x1B[{}m{}\x1B[0m".format(43 if background == BLACK else 47, char or " ")
 
 
+def main():
+    checker = CheckersBoard()
+    print(checker)
+    if sys.platform.startswith("win"):
+        filename = os.path.join(tempfile.gettempdir(), "gameboard.txt")
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(sys.stdout.getvalue())
+        print("wrote '{}'".format(filename), file=sys.__stdout__)
+
+
 class AbstractBoard:
     def __init__(self, rows, columns):
-        self.board = [[None for _ in range(columns) for _ in range(rows)]]
+        self.board = [[None for _ in range(columns)] for _ in range(rows)]
         self.populate_board()
 
     def populate_board(self):
@@ -25,8 +38,44 @@ class AbstractBoard:
         squares = []
         for y, row in enumerate(self.board):
             for x, piece in enumerate(row):
-                squares = console(piece, BLACK if (y+x) % 2 else WHITE)
-                squares.append("\n")
+                square = console(piece, BLACK if (y+x) % 2 else WHITE)
+                squares.append(square)
+            squares.append("\n")
         return "".join(squares)
 
 
+class CheckersBoard(AbstractBoard):
+
+    def __init__(self):
+        super().__init__(10, 10)
+
+    def populate_board(self):
+        for x in range(0, 9, 2):
+            for row in range(4):
+                column = x + ((row + 1) % 2)
+                self.board[row][column] = BlackDraught()
+                self.board[row + 6][column] = WhiteDraught()
+
+
+class Piece(str):
+
+    __slots__ = ()
+
+
+class BlackDraught(Piece):
+
+    __slots__ = ()
+
+    def __new__(Class):
+        return super().__new__(Class, "\N{black draughts man}")
+
+
+class WhiteDraught(Piece):
+
+    __slots__ = ()
+
+    def __new__(Class):
+        return super().__new__(Class, "\N{white draughts man}")
+
+if __name__ == "__main__":
+    main()
